e2f25e4277b8ff51112df31cc563280ed487acd0
Richard <richard.sinaga@ipm.co.id>
2023-01-19 14:00:32 +0700

    filter

--- repository/comic.go
+++ repository/comic.go
@@ -52,3 +52,27 @@ func DeleteComic(db *sql.DB, comic structs.Comic) (err error) {
 
 	return errs.Err()
 }
+
+func FilterComic(db *sql.DB, genre string, tipe string, status bool) (err error, results []structs.Comic) {
+	sql := `SELECT * FROM comic WHERE genre = $1 AND type = $2 AND status = $3`
+
+	rows, err := db.Query(sql, genre, tipe, status)
+	if err != nil {
+		panic(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+		var comic = structs.Comic{}
+
+		err = rows.Scan(&comic.Id, &comic.Title, &comic.Description, &comic.ImageURL, &comic.ReleaseYear, &comic.Genre, &comic.Type, &comic.Status, &comic.CreatedAt, &comic.UpdatedAt)
+		if err != nil {
+			panic(err)
+		}
+
+		results = append(results, comic)
+	}
+
+	return
+}
